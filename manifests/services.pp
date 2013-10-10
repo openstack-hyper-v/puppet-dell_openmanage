77a2f0ef8feb82255bf9a6c053a42180556a8936
Peter Pouliot <ppouliot@gmail.com>
2013-10-09 23:11:16 -0400

    removing typo

--- manifests/services.pp
+++ manifests/services.pp
@@ -1,17 +1,17 @@
 class dell_openmanage::services {
   service {'dsm_om_connsvc':
     ensure  => running,
-    enabled => true,
+    enable  => true,
     require => Class['dell_openmanage::srvadmin'],
   }
   service {'dsm_om_shrsvc':
     ensure  => running,
-    enabled => true,
+    enable  => true,
     require => Class['dell_openmanage::srvadmin'],
   }
   service {'dsm_sa_ipmi':
     ensure  => running,
-    enabled => true,
+    enable  => true,
     require => Class['dell_openmanage::srvadmin'],
   }
 }

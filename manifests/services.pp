62ce6fc31a06f3a7e8d0ceb6786b5584f65e8ab1
Peter Pouliot <ppouliot@gmail.com>
2013-10-09 23:07:00 -0400

    trying to fix service.pp

--- manifests/services.pp
+++ manifests/services.pp
@@ -1,19 +1,16 @@
 class dell_openmanage::services {
-
   service {'dsm_om_connsvc':
-    ensure   => running,
+    ensure  => running,
     enabled => true,
     require => Class['dell_openmanage::srvadmin'],
   }
-
   service {'dsm_om_shrsvc':
-    ensure   => running,
+    ensure  => running,
     enabled => true,
     require => Class['dell_openmanage::srvadmin'],
   }
-
   service {'dsm_sa_ipmi':
-    ensure   => running,
+    ensure  => running,
     enabled => true,
     require => Class['dell_openmanage::srvadmin'],
   }

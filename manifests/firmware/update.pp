4086c66428142e780119847ebee9ce07107c28bf
Peter Pouliot <ppouliot@gmail.com>
2013-10-17 12:50:02 -0400

    fixing typo

--- manifests/firmware/update.pp
+++ manifests/firmware/update.pp
@@ -1,13 +1,13 @@
 class dell_openmanage::firmware::update {
 
   package {'$(bootstrap_firmware)':
-    ensure => latest,
+    ensure  => latest,
     require => Package['dell_ft_install'],
   }
 
   exec {'update-dell-firmware':
-    command => '/usr/sbin/update_firmware --yes',
-    lougoutput => true,
+    command    => '/usr/sbin/update_firmware --yes',
+    logoutput  => true,
     timeout    => 0,
     require    => Package['$(bootstrap_firmware)'],
   }
